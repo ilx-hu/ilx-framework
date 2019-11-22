4d4e166b1dc53855d089367237b2b2c022186ec0
Nagy Attila <nagyatka@gmail.com>
2019-11-22 17:19:52 +0100

    Updated doc, theme module in progress

---
+++ docsrc/source/modules/theme_module.rst
@@ -0,0 +1,20 @@
+Téma modul
+*************
+
+A téma modul célja, hogy alap weboldal sablonokat biztosítson, amelyek kompatibilisek a többi modullal
+
+Függőségek
+=================
+
+Importálás
+========================
+
+
+Paraméterek
+========================
+
+
+Használata
+========================
+
+
---
+++ src/Ilx/Module/Theme/ThemeModule.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Ilx\Module\Theme;
+
+
+use Ilx\Module\IlxModule;
+use Ilx\Module\ModuleManager;
+
+class ThemeModule extends IlxModule
+{
+
+    function defaultParameters()
+    {
+        // TODO: Implement defaultParameters() method.
+    }
+
+    function environmentalVariables()
+    {
+        // TODO: Implement environmentalVariables() method.
+    }
+
+    function routes()
+    {
+        // TODO: Implement routes() method.
+    }
+
+    function serviceProviders()
+    {
+        // TODO: Implement serviceProviders() method.
+    }
+
+    function hooks()
+    {
+        // TODO: Implement hooks() method.
+    }
+
+    function bootstrap(ModuleManager $moduleManager)
+    {
+        // TODO: Implement bootstrap() method.
+    }
+
+    function initScript($include_templates)
+    {
+        // TODO: Implement initScript() method.
+    }
+}
